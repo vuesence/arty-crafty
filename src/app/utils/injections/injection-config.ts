5ce76e65367c01f81020c896e09557a2f74427f9
Ruslan Makarov <ruslan.makarov@gmail.com>
2024-01-04 09:57:26 +0300

    fix: correct path

--- src/app/utils/injections/injection-config.ts
+++ src/app/utils/injections/injection-config.ts
@@ -4,19 +4,19 @@ export const htmlInjectionConfig: IHtmlInjectionConfig = {
   injections: [
     {
       name: "Open Graph",
-      path: "./src/utils/injections/open-graph.html",
+      path: "./src/app/utils/injections/open-graph.html",
       type: "raw",
       injectTo: "head",
     },
     {
       name: "Splash screen",
-      path: "./src/utils/injections/splash-screen.html",
+      path: "./src/app/utils/injections/splash-screen.html",
       type: "raw",
       injectTo: "body-prepend",
     },
     {
       name: "Service worker",
-      path: "./src/utils/injections/sw.js",
+      path: "./src/app/utils/injections/sw.js",
       type: "js",
       injectTo: "head",
     },
