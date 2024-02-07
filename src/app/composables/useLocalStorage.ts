5d4905cff3ff9949cb67b8e7f169832dc51072bc
Ruslan Makarov <ruslan.makarov@gmail.com>
2024-02-07 11:10:19 +0300

    chore: rename some vars

--- src/app/composables/useLocalStorage.ts
+++ src/app/composables/useLocalStorage.ts
@@ -1,12 +1,16 @@
 import { reactive, watch } from "vue";
 
-const data = reactive({});
+// observed reactive data
+const data: Record<string, any> = reactive({});
 
+/**
+ * Initializes the function by adding an event listener to the window for the "storage" event.
+ * When the "arty-crafty-data" key is detected in the event, it parses the new value and updates the data object.
+ */
 function init() {
   window.addEventListener("storage", (e) => {
-    if (e.key === "ap-data") {
-      const d = JSON.parse(e.newValue);
-      for (const [key, value] of Object.entries(d)) {
+    if (e.key === "arty-crafty-data") {
+      for (const [key, value] of Object.entries(JSON.parse(e.newValue))) {
         data[key] = (data[key] instanceof Set) ? new Set(value as []) : value;
       }
     }
@@ -14,11 +18,11 @@ function init() {
 }
 
 watch(data, () => {
-  const serializedData = {};
+  const decomposedData = {};
   for (const key in data) {
-    serializedData[key] = (data[key] instanceof Set) ? Array.from(data[key]) : data[key];
+    decomposedData[key] = (data[key] instanceof Set) ? Array.from(data[key]) : data[key];
   }
-  localStorage.setItem("ap-data", JSON.stringify(serializedData));
+  localStorage.setItem("ap-data", JSON.stringify(decomposedData));
 });
 
 export function useLocalStorage() {
@@ -26,8 +30,5 @@ export function useLocalStorage() {
     data[key] = value;
   }
 
-  return {
-    init,
-    observe,
-  };
+  return { init, observe };
 }
