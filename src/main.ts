8c54feb5d67f47d4fef1bd319aa3fda93b48f1cd
Ruslan Makarov <ruslan.makarov@gmail.com>
2024-03-12 11:30:52 +0300

    test: add web-vital metrics

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
 import "./app/assets/styles/base.css";
 import "./app/assets/styles/custom.css";
+import { onFCP, onFID, onLCP } from "web-vitals";
 import App from "./app/App.vue";
 import { router } from "./app/router";
 import { init } from "@/app/utils/init";
+
+onLCP(console.log);
+onFCP(console.log);
+onFID(console.log);
 
 const app = createApp(App);
 app.use(router);
